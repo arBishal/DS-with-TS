4a64a1ff8ee37818a29236a5829bf784e9b5ce88
arBishal <m.arbishal@gmail.com>
2025-11-29 01:16:29 +0600

    update: show use of linkedlist

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,20 @@ import { LinkedList } from './linked-list/LinkedList';
 
 // Example usage of LinkedList
 const linkedList = new LinkedList();
-linkedList.add(1);
-linkedList.add(2);
-linkedList.add(3);
-console.log('Linked List:', linkedList.traverse());
+
+linkedList.insertAtEnd(0);
+linkedList.insertAtEnd(3);
+linkedList.insertAtEnd(8);
+console.log('List elements:', linkedList.traverse()); // [0, 3, 8]
+
+linkedList.removeFromEnd();
+console.log('List elements after removal:', linkedList.traverse()); // [0, 3]
+
+linkedList.insertAtPosition(1, 1);
+linkedList.insertAtPosition(3, 4);
+console.log('List elements after inserting 1 at position 1:', linkedList.traverse()); // [0, 1, 3, 4]
+
+linkedList.removeFromPosition(2);
+console.log('List elements after removing from position 2:', linkedList.traverse()); // [0, 1, 4]
+
+console.log('Size of the list:', linkedList.getSize()); // 2
